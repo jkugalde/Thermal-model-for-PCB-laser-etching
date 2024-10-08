0e2b8adc1c5caf19a59dfd2ef24f8368519d5520
jkugalde <jcugaldecas@gmail.com>
2024-10-08 15:53:01 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Thermal-model-for-PCB-laser-etching
-Matlab simulations for laser etching tracks in PCBs
+# Thermal model for PCB laser etching
+
+## Context
+
+A PCBs is a printed circuit board, which usually is the part of an electronic/mechatronic system that serves as a chasis for electronics and its connections. The PCB is usually a copper plated non conductive and rigid material, where the copper is removed to generate the connection paths. The copper can be removed trough milling (cutting) or through chemical etching.
+
+Laser etching is a process that removes paint previously applied on the copper surface of a PCB. The PCB is then exposed to a chemical that removes the exposed copper, generating the paths.
+
+This is part of the laboratory project in the course Advanced Manufacturing Processes B at POLIMI, the idea is to procude custom PCBs on demand. We will use Matlab to generate a point heat source model, the results will be compared and adjusted based on experiments done in a Snapmaker 2.0, in order to obtain optimal process parameters.
+
+### 3D Heat point source model 
+
+Heat propagation in $x,y,z$ directions from a point in the material, quasi-stationary considering relative coordinates moving with the laser in direction $\xi$ with speed $v$. Supposes:
+
+- No convection or radiation
+- Constant ambient temperature $T_{0}$
+- Steady state conditions
+- Homogeneous and isotropic material
+- Constant thermal properties
+
+
+ $ T(\xi , y , z) = T_{0}+ \eta \cdot \frac{P}{2 \pi k r} e^{\frac{-v}{2 \alpha}(\xi +r)}$
+
+Where:
+
+- $\eta$ is the process efficiency
+- $k$ conductivity of the material ($W/mK$)
+- $\alpha=k/\rho c_{p}$ is the thermal diffusivity ($m^{2}/s$)
+
+The radius $r$ is defined as:
+
+$r=\sqrt( \xi^{2} + y^{2} + (c_{z}z)^{2} ) $
+
+$c_{z}$ is an empirical coefficient in the workpiece.
+
+The ablation of the material will be considered in every section where the material is greather or equal than its vaporization temperature $T_{vap}$.
+
+### Material and process parameters
+
+We will be using a resin with the following properties:
+
+- $k = 0.3$ $W/mK$
+- $\rho=1450$ $kg/m^3$ 
+- $T_{vap}=300$ $°C$ 
+- $c_{p}=1450$ $J/kgK$ 
+
+The process parameters are:
+
+- $v=800$ $mm/min$
+- $P=1600$ $mW$
+- $c_{z}=10$
+- $\eta=0.3$
+
+### Results
+
+The current simulated path width is $181 um$.
+
+<img img="/images/xy.png" width="600">
+<img img="/images/xz.png" width="600">
+<img img="/images/yz.png" width="600">
+<img img="/images/contour.png" width="600">
+<img img="/images/width.png" width="600">
---
+++ src/Lab1AMP.m
@@ -0,0 +1,136 @@
+clear; clc; close all;
+tic
+% Parameters for paint
+
+rho = 1450; % Material density kg/m3
+cp=2500; % Specific heat J/kgK
+k = 0.3; % Thermal conductivity W/mK
+Tvap = 300;% Vaporization temperature C
+Abs = 0.3;  % Absorption coefficient
+thicc = 20*10^-6; % Thickness in um
+P = 1600*10^-3; % Irradiation in Watts
+v = 800/60*10^-3; % Speed of source m/s
+N=1; % Number of passes
+cz=10; % Geometrical correction factor
+nu = 0.3; % Process efficiency
+alpha=k/(rho*cp); % Thermal diffusivity m2/s
+T0 = 25; % Initial material temperature
+
+% Domain (in micrometers)
+
+delta=1;
+ymin=-200;
+ymax=200;
+xmin=-1000;
+xmax=50;
+zmin=0;
+zmax=20;
+x=linspace(xmin,xmax,(xmax-xmin)/delta)*10^-6; % to meters
+y=linspace(ymin,ymax,(ymax-ymin)/(delta))*10^-6;
+z=linspace(zmin,zmax,(zmax-zmin)/delta)*10^-6;
+nx=length(x);
+ny=length(y);
+nz=length(z);
+
+Tmat=zeros(nx,ny,nz);
+
+%Calculation
+
+for ii=1:nx
+    for jj=1:ny
+        for kk=1:nz
+            r=sqrt(x(ii)^2+y(jj)^2+(cz*z(kk))^2);
+            Tmat(ii,jj,kk)=T0+nu*(P/(2*pi*k*r))*exp(-v/(2*alpha)*(x(ii)+r));
+            if Tmat(ii,jj,kk)>Tvap
+                Tmat(ii,jj,kk)=Tvap;
+            else
+                % TmatLim(ii,jj,kk)=Tmat(ii,jj,kk);
+            end
+        end
+    end
+
+end
+
+figure
+
+[X,Y]=meshgrid(x*10^6,y*10^6);
+pcolor(X,Y,transpose(Tmat(:,:,1)),EdgeColor="none")
+grid off
+title('Tlim(\xi,y)')
+xlim([xmin xmax]);
+ylim([ymin ymax]);
+xlabel('\xi[\mum]')
+ylabel('y[\mum]')
+ax = gca;
+ax.FontSize = 13;
+c = colorbar;
+clim([25 300])
+c.Label.String = 'T[°C]';
+
+figure
+
+[X,Z]=meshgrid(x*10^6,z*10^6);
+pcolor(X,Z,transpose(squeeze(Tmat(:,ny/2,:))),EdgeColor="none")
+title('Tlim(\xi,z)')
+xlim([xmin xmax]);
+ylim([zmin zmax]);
+xlabel('\xi[\mum]')
+ylabel('z[\mum]')
+ax = gca;
+ax.FontSize = 13;
+c = colorbar;
+clim([25 300])
+c.Label.String = 'T[°C]';
+
+figure
+
+[Y,Z]=meshgrid(y*10^6,z*10^6);
+pcolor(Y,Z,transpose(squeeze(Tmat(1,:,:))),EdgeColor="none")
+title('Tlim(y,z)')
+xlim([ymin ymax]);
+ylim([zmin zmax]);
+xlabel('y[\mum]')
+ylabel('z[\mum]')
+ax = gca;
+ax.FontSize = 13;
+c = colorbar;
+clim([25 300])
+c.Label.String = 'T[°C]';
+
+figure
+
+[X,Y]=meshgrid(x*10^6,y*10^6);
+contour(X,Y,transpose(Tmat(:,:,1)),'ShowText','on');
+title('Tlim(\xi,y)')
+xlim([xmin xmax]);
+ylim([ymin ymax]);
+xlabel('\xi[\mum]')
+ylabel('y[\mum]')
+ax = gca;
+ax.FontSize = 13;
+
+figure
+
+[X,Y]=meshgrid(x*10^6,y*10^6);
+[M,C]=contour(X,Y,transpose(Tmat(:,:,1)),[300 300],'ShowText','on');
+M(:,1)=[];
+xcords=M(1,:);
+ycords=M(2,:);
+width0=min(ycords);
+width1=max(ycords);
+width=round(width1-width0);
+xlim([xmin xmax]);
+ylim([ymin ymax]);
+xlabel('\xi[\mum]')
+ylabel('y[\mum]')
+ax = gca;
+ax.FontSize = 13;
+title(['Tlim(\xi,y) VAP width: ',num2str(width),'\mum'])
+hold on
+x1=xcords(1);
+x2=xcords(size(xcords,2));
+xp = [x1 x2];
+yp = [width0 width1];
+plot(xp,yp)
+legend('VAP contour','VAP w_{sim}')
+toc
